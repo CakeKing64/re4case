57e0dfbcf49a5743c616a28f83fcdb7fbd6d0d0f
CakeKing64 <cakeking64@gmail.com>
2024-12-01 10:56:24 +1000

    Me when I remove debug stuff

--- lua/playercase/server/sv_hooks.lua
+++ lua/playercase/server/sv_hooks.lua
@@ -8,21 +8,3 @@ hook.Add("Tick", "CASE_ServerTick", function ()
         end
     end
 end)
-
-hook.Add("PlayerSay", "AAAAA_C", function (ply, text, team)
-    if text == "debug" then
-        for k, v in ipairs(CaseInventory.ItemRegister) do
-            PrintTable(v)
-        end
-    end
-    local n = tonumber(text)
-    if n ~= nil then
-        local nm = game.GetAmmoName(n)
-        PrintMessage(HUD_PRINTTALK, nm)
-        ply:GiveAmmo(5, n)
-    end
-    n = game.GetAmmoID(text)
-    if n ~= -1 then
-        PrintMessage(HUD_PRINTTALK, string.format("%s -> %i", text, n))
-    end
-end)
