f143ad61f81dd6bb59ff7197d72cbd88d2079194
CakeKing64 <cakeking64@gmail.com>
2024-12-04 00:13:40 +1000

    Make sure the loadout is setup after registering items (Regressionma)

--- lua/playercase/shared/sh_hooks.lua
+++ lua/playercase/shared/sh_hooks.lua
@@ -12,4 +12,10 @@ hook.Add("InitPostEntity", "CASE_InitPostEntity", function ()
     for k, v in ipairs(player.GetAll()) do
         CaseInventory:Sync(v)
     end
+
+    -- Make sure everything is placed
+    if CLIENT then
+        CaseInventory:RefreshLoadout(CaseInv())
+    end
+    
 end)
