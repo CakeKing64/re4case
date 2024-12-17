6fba7ab1286e37a2da87e94a2f4346ed93e7ae71
CakeKing64 <cakeking64@gmail.com>
2024-12-18 04:29:18 +1000

    Make sure the player's hands don't vanish when holding items

--- lua/playercase/server/sv_player.lua
+++ lua/playercase/server/sv_player.lua
@@ -248,6 +248,7 @@ hook.Add("OnPlayerPhysicsPickup", "CASE_OnPlayerPhysicsPickup", function( ply, e
 
             -- Add to a queue to be added to the inventory next tick
             -- If the item is invalid by then it will be assumed to have been used
+            ply:DropObject()
             table.insert(CaseInventory.PickupQueue, {Player=ply, ENT=ent, Timer=0, ItemID=itemID})
         end
     end
